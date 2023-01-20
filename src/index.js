ca91eb659b1f3b0c40b5a5eae521d32ac9a754e9
Paul Treanor <treanorpaul9@gmail.com>
2023-01-20 10:00:18 +0000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./styles.css";
+
+const DASHBOARD_URL =
+  "https://vdix0nrox3.execute-api.eu-west-1.amazonaws.com/prod/dashboard/d733b0b2-f429-4bdf-82ab-c9fc3b3190d7";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      dashboard: {}
+    };
+    this.refreshData = this._refreshData.bind(this);
+  }
+
+  async _refreshData() {
+    const resp = await fetch(DASHBOARD_URL, {
+      method: "GET",
+      mode: "cors"
+    });
+    const dashboard = await resp.json();
+    this.setState({
+      loading: false,
+      dashboard
+    });
+    setTimeout(this.refreshData, 10 * 1000);
+  }
+
+  async componentDidMount() {
+    this.refreshData();
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <div>Loading...</div>;
+    }
+
+    const d = this.state.dashboard;
+
+    return (
+      <div className="App">
+        <h1>{d.name}</h1>
+        {d.widgets.map((widget) => (
+          <details open key={widget.id}>
+            <summary>{widget.name}</summary>
+            <div className="infoPanel">
+              {widget.realtimeInfo.map((info) => (
+                <div
+                  className="infoItem"
+                  key={`${info.arrivingInMinutes}-${info.destination}`}
+                >
+                  <span className="arrivingInMinutes">
+                    {info.arrivingInMinutes}m
+                  </span>{" "}
+                  <span className="destination">{info.destination}</span>
+                </div>
+              ))}
+            </div>
+          </details>
+        ))}
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
